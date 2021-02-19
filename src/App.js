b321598e501b777b5af5bd8ac012f5c02c67341d
Engjell <40763@edu.raseko.fi>
2021-02-19 21:18:53 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
 import NewsCards from "./Components/NewsCards/NewsCards";
+import useStyles from "./styles";
 const alanKey =
   "f5741b80627ea9205b2edf940b2e8dc62e956eca572e1d8b807a3e2338fdd0dc/stage";
 
 const App = () => {
   const [newsArticles, setNewsArticles] = useState([]);
-
+  const classes = useStyles();
   useEffect(() => {
     alanBtn({
       key: alanKey,
@@ -19,7 +20,13 @@ const App = () => {
   }, []);
   return (
     <div>
-      <h1>News</h1>
+      <div className={classes.logoContainer}>
+        <img
+          src="https://ak.picdn.net/shutterstock/videos/7613365/thumb/1.jpg"
+          className={classes.alanLogo}
+          alt="alan logo"
+        />
+      </div>
       <NewsCards articles={newsArticles} />
     </div>
   );
